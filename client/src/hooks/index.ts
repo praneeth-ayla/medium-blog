deb88b2c6dc516c4b6689b04e69550a0b5b71c9e
Praneeth <praneethayla@gmail.com>
2024-04-05 17:37:49 +0530

    Quote api and display

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -127,3 +127,18 @@ export function useUserDetails(token: any): any {
 
 	return JSON.parse(jsonPayload);
 }
+
+export async function useRandomQuote() {
+	try {
+		const response = await axios.get("https://type.fit/api/quotes");
+		const quotes = response.data;
+
+		const randomIndex = Math.floor(Math.random() * quotes.length);
+		const randomQuote = quotes[randomIndex];
+
+		return randomQuote;
+	} catch (error) {
+		console.error("Error fetching random quote:", error);
+		return null;
+	}
+}
